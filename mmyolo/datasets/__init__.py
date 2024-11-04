c4ebbab56b4464031fc6bf83a7344ef45e8f5145
maoxiaoming86 <mxmhhh@163.com>
2024-11-04 13:53:47 +0800

    add CocoDatasetEx，为了支持ignore_labels

--- mmyolo/datasets/__init__.py
+++ mmyolo/datasets/__init__.py
@@ -5,8 +5,10 @@ from .yolov5_coco import YOLOv5CocoDataset
 from .yolov5_crowdhuman import YOLOv5CrowdHumanDataset
 from .yolov5_dota import YOLOv5DOTADataset
 from .yolov5_voc import YOLOv5VOCDataset
+from .coco import CocoDatasetEx
 
 __all__ = [
     'YOLOv5CocoDataset', 'YOLOv5VOCDataset', 'BatchShapePolicy',
-    'yolov5_collate', 'YOLOv5CrowdHumanDataset', 'YOLOv5DOTADataset'
+    'yolov5_collate', 'YOLOv5CrowdHumanDataset', 'YOLOv5DOTADataset',
+    'CocoDatasetEx'
 ]
